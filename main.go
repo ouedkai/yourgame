03bc20dfd99b545fa970a7c44a6124b614c1b852
Ouederni Kais <kais.ouederni@edu.hefr.ch>
2023-11-15 16:15:36 +0100

    dsdasda

--- main.go
+++ main.go
@@ -1,35 +1,35 @@
+// game.go
 package yourgame
 
 import (
-	"image/color"
-
 	"github.com/hajimehoshi/ebiten/v2"
-	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
-	"github.com/hajimehoshi/ebiten/v2/inpututil"
 )
 
-// Game struct represents your game and implements the ebiten.Game interface.
-type Game struct {
+// Game interface represents the game contract.
+type Game interface {
+	Update() error
+	Draw(screen *ebiten.Image)
+	Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int)
+}
+
+// GameImpl struct implements the Game interface.
+type GameImpl struct {
 	x, y float64
 }
 
-// Update is called every frame (60 times per second) to update the game state.
-func (g *Game) Update() error {
-	// Move the square to the right when the screen is touched or the left mouse button is pressed.
-	if inpututil.IsMouseButtonJustPressed(ebiten.MouseButtonLeft) {
-		g.x += 10
-	}
-
+// Update implements the Game interface.
+func (g *GameImpl) Update() error {
+	// Implement update logic here
 	return nil
 }
 
-// Draw is called every frame (60 times per second) to draw the game.
-func (g *Game) Draw(screen *ebiten.Image) {
-	// Draw a square at the current position (g.x, g.y).
-	ebitenutil.DrawRect(screen, g.x, g.y, 50, 50, color.White)
+// Draw implements the Game interface.
+func (g *GameImpl) Draw(screen *ebiten.Image) {
+	// Implement draw logic here
 }
 
-// Layout is called whenever the window size is changed (but we don't use it in this example).
-func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
-	return 320, 240 // Fixed screen size in this example
+// Layout implements the Game interface.
+func (g *GameImpl) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
+	// Implement layout logic here
+	return 320, 240
 }
